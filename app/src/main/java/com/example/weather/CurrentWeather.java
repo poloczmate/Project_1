e976f8be1a51255d1385343eaa8b2c55f90d5983
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2021-08-30 22:26:30 +0200

    first running version

--- app/src/main/java/com/example/weather/CurrentWeather.java
+++ app/src/main/java/com/example/weather/CurrentWeather.java
@@ -5,6 +5,10 @@ import org.json.*;
 
 public class CurrentWeather {
     //class to access current weather data for any location including over 200,000 cities
+    private double KtoC(double kelvin){
+        kelvin -= 272.15;
+        return (double) Math.round(kelvin * 100) / 100; //round it to 2 decimal
+    }
 
     private String getUpdate(String city){
         String response = null;
@@ -18,6 +22,87 @@ public class CurrentWeather {
         }
         //TODO improve Errorhandling
         return response;
+    }
+
+    public String getWeather(String city){
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(getUpdate(city));
+            JSONArray arr = obj.getJSONArray("weather");
+            return arr.getJSONObject(0).getString("main") + ", " + arr.getJSONObject(0).getString("description");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getWind(String city){
+        JSONObject obj = null;
+        String toReturn = "Speed: ";
+        try {
+            obj = new JSONObject(getUpdate(city));
+            toReturn += obj.getJSONObject("wind").getString("speed") + " direction: ";
+            double degrees = obj.getJSONObject("wind").getDouble("deg");
+            if (degrees >= 348.75 || degrees <= 11.25){
+                toReturn += "N";
+            }else if(degrees >= 11.25 || degrees <= 33.75){
+                toReturn += "NNE";
+            }else if(degrees >= 33.75 || degrees <= 56.25){
+                toReturn += "NE";
+            }else if(degrees >= 56.25 || degrees <= 78.75){
+                toReturn += "ENE";
+            }else if(degrees >= 78.75 || degrees <= 101.25){
+                toReturn += "E";
+            }else if(degrees >= 101.25 || degrees <= 123.75){
+                toReturn += "ESE";
+            }else if(degrees >= 123.75 || degrees <= 146.25){
+                toReturn += "SE";
+            }else if(degrees >= 146.25 || degrees <= 168.75){
+                toReturn += "SSE";
+            }else if(degrees >= 168.75 || degrees <= 191.25){
+                toReturn += "S";
+            }else if(degrees >= 191.25 || degrees <= 213.75){
+                toReturn += "SSW";
+            }else if(degrees >= 213.75 || degrees <= 236.25){
+                toReturn += "SW";
+            }else if(degrees >= 236.25 || degrees <= 258.75){
+                toReturn += "WSW";
+            }else if(degrees >= 258.75 || degrees <= 281.25){
+                toReturn += "W";
+            }else if(degrees >= 281.25 || degrees <= 303.75){
+                toReturn += "WNW";
+            }else if(degrees >= 303.75 || degrees <= 326.25){
+                toReturn += "NW";
+            }else if(degrees >= 326.25 || degrees <= 348.75){
+                toReturn += "NW";
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return toReturn;
+    }
+
+    public String getCelsius(String city){
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(getUpdate(city));
+            double temp = KtoC(obj.getJSONObject("main").getDouble("temp"));
+            return String.valueOf(temp);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ""; //-250 = error
+    }
+    public String getFeelsLike(String city){
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(getUpdate(city));
+            double temp = KtoC(obj.getJSONObject("main").getDouble("feels_like"));
+            return String.valueOf(temp);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ""; //-250 = error
     }
 
 
--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
@@ -17,7 +18,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click(View v) throws InterruptedException {
+        TextView we = (TextView) findViewById(R.id.weather);
+        TextView te = (TextView) findViewById(R.id.temp);
+        TextView wi = (TextView) findViewById(R.id.wind);
+        TextView tv = (TextView) findViewById(R.id.cityName);
+        String city = tv.getText().toString();
+
         CurrentWeather CW = new CurrentWeather();
-        System.out.println(CW.getNews("London"));
+        we.setText(CW.getWeather(city));
+        te.setText("Celsius: " + CW.getCelsius(city) + ", feels like: " + CW.getFeelsLike(city) + " celsius");
+        wi.setText(CW.getWind(city));
     }
 }
